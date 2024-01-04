ddb29714c0947f3badd4d14a4fca6fdc8b82bcf6
Ghulam Taha <154999123+Ghulam-Taha@users.noreply.github.com>
2024-01-04 07:04:30 +0000

    Vowel or Consonant

---
+++ print.dart
@@ -0,0 +1,12 @@
+void mian (){
+var alphabet = 'D'
+    if(alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u') {
+        print("The Given Word is Vowel")
+    } else{
+        print("The Given Word is Consonant")
+    }
+
+
+
+
+}
